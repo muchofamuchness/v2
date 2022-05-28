246bec7804deef97fa0b7eadd1f474e130d4ae5f
Shahar. Z <Shaharzfrn@gmail.com>
2022-05-28 15:00:18 +0300

    add alphabeta

--- submission.py
+++ submission.py
@@ -133,10 +133,93 @@ class AgentMinimax(Agent):
         print(f"Run time: {time.time() - start}")
         return env.get_legal_operators(agent_id)[operator.value]
 
+
 class AgentAlphaBeta(Agent):
+
+
+
+    def __init__(self):
+        self.alpha = -math.inf
+        self.beta = math.inf
+
     # TODO: section c : 1
+    def heuristic(self, env: TaxiEnv, taxi_id: int):
+        reward = 0
+
+        taxi = env.get_taxi(taxi_id)
+        other_taxi = env.get_taxi((taxi_id + 1) % 2)
+
+        reward += (taxi.cash - other_taxi.cash) * 10  # more / less reward?
+        reward += taxi.fuel * 5
+        closest_station_id = 0 if (manhattan_distance(env.gas_stations[0].position, taxi.position) <=
+                                   manhattan_distance(env.gas_stations[1].position, taxi.position)) else 1
+
+        destination = None
+        if len([(manhattan_distance(passenger.position, taxi.position), i) for i, passenger in
+                enumerate(env.passengers)]) == 0:
+            destination = taxi.passenger.destination
+
+        else:
+            closest_passenger = min([(manhattan_distance(passenger.position, taxi.position), i) for i, passenger in
+                                     enumerate(env.passengers)], key=lambda x: x[0])
+
+            destination = taxi.passenger.destination if (env.taxi_is_occupied(taxi_id)) \
+                else env.passengers[closest_passenger[1]].position
+
+        if (taxi.fuel < manhattan_distance(taxi.position, destination) +
+                min([manhattan_distance(destination, station.position) for station in env.gas_stations])):
+            reward += 8 - manhattan_distance(taxi.position, env.gas_stations[closest_station_id].position)
+        else:
+            reward += 8 - manhattan_distance(taxi.position, destination)
+        return reward
+
+    def return_value(self, a):
+        if isinstance(a, int):
+            return a
+        return a[0]
+
+    def create_child(self, child, taxi_id, op):
+        child.apply_operator(taxi_id, op)
+        return child
+
+    def alphaBeta(self, env, agent, turn, depth):
+        if env.done() or depth == 0:
+            return self.heuristic(env, agent)
+
+        operators = env.get_legal_operators(turn)
+        children = [self.create_child(env.clone(), turn, op) for op in operators]
+
+        children_heuristics = [(self.return_value(self.alphaBeta(child, agent, 1-turn, depth - 1)), op) for child, op in zip(children, operators)]
+
+        if turn == agent:
+            current_max = max(children_heuristics, key=lambda x: x[0])
+            self.alpha = max(current_max[0], self.alpha)
+            current_max = None if current_max >= self.beta else current_max
+            return current_max
+        else:
+            current_min = min(children_heuristics, key=lambda x: x[0])
+            self.beta = min(current_min[0], self.beta)
+            current_min = None if current_min <= self.alpha else current_min
+            return current_min
+
+
+    def process_run(self, env, agent_id, operator):
+        depth = 1
+        while True:
+            result = self.alphaBeta(env, agent_id, agent_id, depth)[1]
+            operator.value = env.get_legal_operators(agent_id).index(result)
+
+            depth += 1
+
     def run_step(self, env: TaxiEnv, agent_id, time_limit):
-        raise NotImplementedError()
+        operator = multiprocessing.Value('i', 0)
+        start = time.time()
+        proc = multiprocessing.Process(target=self.process_run, args=(env, agent_id, operator))
+        proc.start()
+        proc.join(time_limit * 0.9)
+        proc.terminate()
+        print(f"Run time: {time.time() - start}")
+        return env.get_legal_operators(agent_id)[operator.value]
 
 
 class AgentExpectimax(Agent):
